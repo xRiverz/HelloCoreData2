ef44432d411eda30666c1bb5bb8ab9c9fa1db2d3
xRiverz <anhargalam@gmail.com>
2022-01-04 17:30:45 +0000

    00

---
+++ HelloCoreData/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  HelloCoreData
+//
+//  Created by administrator on 04/01/2022.
+//
+
+import Foundation
+
+class User : AnyClass {
+    let user = User(context: managedObjectContext)
+}
--- HelloCoreData/ViewController.swift
+++ HelloCoreData/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
+let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+
 class ViewController: UIViewController {
 
+    @IBAction func saveUser(_ sender: UIButton) {
+        HelloCoreData.saveUser()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+func saveUser() {
+    let user = User(context: managedObjectContext)
+    // Under the 'saveUser' method, after the 'let user = ....' statement
+    user.firstname = "Bob"
+    user.email = "Bob@bobmail.com"
+    if managedObjectContext.hasChanges {
+        //What do you think would trigger the  catch statement below? 
+        do {
+            try managedObjectContext.save()
+            print("Success!")
+        } catch {
+            print("error \(error)")
+        }
+    }
+    
 }
 
